4cf226ae2394f01aa7ed78e9383c300e75d9ce66
Vyacheslav Polyansky <polsha13esma@mail.ru>
2024-06-30 17:31:19 +0700

    Добавьте файлы проекта.

---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using DNet_V3_Tutorial.Log;
+
+namespace TutorialBot.Log
+{
+  public class ConsoleLogger : Logger
+  {
+    // Override Log method from ILogger, passing message to LogToConsoleAsync()
+    public override async Task Log(LogMessage message)
+    {
+      // Using Task.Run() in case there are any long running actions, to prevent blocking gateway
+      Task.Run(() => LogToConsoleAsync(this, message));
+    }
+
+    private async Task LogToConsoleAsync<T>(T logger, LogMessage message) where T : DNet_V3_Tutorial.Log.ILogger
+    {
+      Console.WriteLine($"guid:{_guid} : " + message);
+    }
+  }
+}
---
+++ Modules/InteractionModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialBot.Modules
+{
+  public class InteractionModule:InteractionModuleBase<SocketInteractionContext>
+  {
+    [SlashCommand("ping","Receive a ping messege!")]
+
+    public async Task HandlePingCommand()
+    {
+      await RespondAsync("PONG!");
+    }
+  }
+}
